ba4ca568d10776d236c99a02018976f0518cb01a
raj-18110 <sunandan.raj18110@gmail.com>
2017-06-29 18:01:11 +0530

    shell script for initial host machine configuration

---
+++ src/cluster-steps.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+$password=vlead123
+
+sed -i '/proxy=.*/d' /etc/yum.conf
+echo 'proxy=http://proxy.iiit.ac.in:8080' >> /etc/yum.conf
+
+
+yum install epel-release -y
+yum install ansible -y
+yum install emacs -y
+yum install sshpass -y
+
+
+ssh-keygen -f id_rsa -t rsa -N ''
+sshpass -p $password ssh-copy-id root@localhost
+sshpass -p $password ssh-copy-id root@127.0.0.1
+
